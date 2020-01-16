4e5497d382b40299025ba7a2fd61689a8fe03938
losanov <losanov@ukr.net>
2020-01-16 11:46:44 +0200

    2nd commit

---
+++ js.js
@@ -0,0 +1,51 @@
+let num = 50;
+
+if (num < 49) {
+    console.log("neverno")
+} else if (num > 100) {
+    console.log("mnogo")
+} else {
+    console.log("verno")
+}
+
+(num = 50) ? console.log("verno"): console.log("neverno");
+
+
+switch (num) {
+    case num < 49:
+        console.log("neverno");
+        break;
+    case num > 100:
+        console.log("mnogo");
+        break;
+    case num > 80:
+        console.log("i eto mnogo");
+        break;
+    case 50:
+        console.log("verno");
+        break;
+    default:
+        console.log("vse ne tak");
+        break;
+
+}
+
+// let num = 50;
+
+// while (num < 55) {
+//     console.log (num);
+//     num++;
+// }
+
+// do {
+//         console.log(num);
+//         num++;
+// }
+// while (num <55);
+
+for (i = 1; i < 8; i++) {
+    if (i==6) {
+        continue;
+    }
+    console.log(i);
+}
